40e7a6725ee311689276a6e0ba930c76fade5f9c
vigilanteX <dixitaman8@gmail.com>
2025-06-26 14:32:07 +0530

    Tags- "not @Tagname" excludes this tag

--- src/test/java/runners/TestNGRunner.java
+++ src/test/java/runners/TestNGRunner.java
@@ -3,7 +3,7 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features/Tags",glue = "stepdefinitions",tags = "@Smoke")
+@CucumberOptions(features = "src/test/resources/features/Tags",glue = "stepdefinitions",tags = "not @Smoke")
 public class TestNGRunner extends AbstractTestNGCucumberTests
 {
 }
