6df69dec14a094d6929305310e33d84b49801827
vigilanteX <dixitaman8@gmail.com>
2025-06-27 22:24:41 +0530

    Added plugin "pretty to make console look good"

--- src/test/java/runners/TestNGRunner.java
+++ src/test/java/runners/TestNGRunner.java
@@ -3,7 +3,10 @@ package runners;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
-@CucumberOptions(features = "src/test/resources/features/DataTypes/dataTypes.feature",glue = "stepdefinitions.DataType")
+@CucumberOptions(features = "src/test/resources/features/DataTypes/dataTypes.feature",glue = "stepdefinitions.DataType",
+plugin = {
+        "pretty"
+})
 public class TestNGRunner extends AbstractTestNGCucumberTests
 {
 }
